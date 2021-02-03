b8f89dea7cc017a545f4d9fa9eede7c96f239d3d
Micah Whitsel <micah.whitsel@gmail.com>
2021-02-02 21:34:52 -0900

    Adding arsenal functions library

---
+++ 7Cav-Insurgency.lythium/scripts/CfgFunctions.hpp
@@ -0,0 +1,15 @@
+class scripts {
+    class arsenal {
+        file = "Scripts\arsenal";
+        class arsenalGeneral {};
+        class arsenalInfantry {};
+        class arsenalLongshot {};
+        class arsenalMedic {};
+        class arsenalLogistics {};
+        class arsenalTanker {};
+        class arsenalHeliPilot {};
+        class arsenalHeliCrew {};
+        class arsenalPlanePilot {};
+        class arsenalAdmin {};
+    }
+}
